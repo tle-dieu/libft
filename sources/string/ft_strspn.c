559143e2dc62cd22d07d15849f1d4402648b0656
Thomas Ubuntu <thomas.lddv@orange.fr>
2019-04-25 19:02:34 +0200

    correction ft_strspn

--- sources/string/ft_strspn.c
+++ sources/string/ft_strspn.c
@@ -6,7 +6,7 @@
 /*   By: tle-dieu <tle-dieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 20:26:52 by tle-dieu          #+#    #+#             */
-/*   Updated: 2019/04/22 15:49:43 by tle-dieu         ###   ########.fr       */
+/*   Updated: 2019/04/25 19:02:24 by tle-dieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ size_t	ft_strspn(const char *s, const char *accept)
 	while (s[i])
 	{
 		tmp = accept;
-		while (*tmp++ != s[i])
-			if (!*tmp)
+		while (*tmp != s[i])
+			if (!*tmp++)
 				return (i);
 		i++;
 	}
